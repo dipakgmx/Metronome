e0d55a7f854cf9d63f5ce9f8b3a93e50e22bc7e0
Dipak <shetty.dipak@gmx.com>
2019-05-11 17:35:56 +0200

    fix leaks

--- main.cpp
+++ main.cpp
@@ -1,11 +1,9 @@
 #include "MainWindow.h"
 #include <QApplication>
 
-
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
-
     //Check if media files actually exist
     if (  (!QFile(":/MediaFiles/high.mp3").exists())
         ||(!QFile(":/MediaFiles/high.mp3").exists()))
@@ -16,12 +14,11 @@ int main(int argc, char *argv[])
         msgBox.exec();
         return 1;
     }
-
     QStyle* fusion = QStyleFactory::create("Fusion");
     QApplication::setStyle(fusion);
-    MainWindow window;
-    window.resize(400, 200);
-    window.setWindowTitle("Metronome");
-    window.show();
+    MainWindow* window = new MainWindow();
+    window->resize(400, 200);
+    window->setWindowTitle("Metronome");
+    window->show();
     return QApplication::exec();
 }
